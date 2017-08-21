12a70e2ba2e20e053eb8835da5dc5b5c108dad43
liujinlong <liujinlong@jiudaotech.com>
2017-08-21 19:12:42 +0800

    OAuth2.0授权认证服务示例

---
+++ src/main/java/org/playchain/demo3/DemoApplication.java
@@ -0,0 +1,32 @@
+package org.playchain.demo3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+@EnableAuthorizationServer
+public class DemoApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		logger.info(user.toString());
+		return user;
+	}
+
+}
