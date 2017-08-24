d4b3dd01bb9f2967245503639fa78e4132639c5d
liujinlong <liujinlong@jiudaotech.com>
2017-08-24 13:15:36 +0800

    security配置

---
+++ src/main/java/org/playchain/demo3/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package org.playchain.demo3.config;
+
+import org.playchain.demo3.filter.MyFilterSecurityInterceptor;
+import org.playchain.demo3.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by Liu on 2017/8/24.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyFilterSecurityInterceptor myFilterSecurityInterceptor;
+
+    @Bean
+    UserDetailsService customUserService() {
+        return new UserService();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        @formatter:off
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error")
+                .permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .csrf().disable();
+    }
+}
