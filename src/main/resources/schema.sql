8797284fc2bd1640af619a6558092f4b986efd04
liujinlong <liujinlong@jiudaotech.com>
2017-08-24 16:48:40 +0800

    更新脚本

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-08-24 13:00:21
+Date: 2017-08-24 16:43:40
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,13 +25,18 @@ CREATE TABLE `permission` (
   `url` varchar(255) DEFAULT NULL,
   `pid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of permission
 -- ----------------------------
-INSERT INTO `permission` VALUES ('1', 'HOME', '/home', null);
+INSERT INTO `permission` VALUES ('1', 'USER', '/home', null);
 INSERT INTO `permission` VALUES ('2', 'ADMIN', '/admin', null);
+INSERT INTO `permission` VALUES ('3', 'USER', '/user1', null);
+INSERT INTO `permission` VALUES ('4', 'USER', '/user2', null);
+INSERT INTO `permission` VALUES ('5', 'USER', '/user3', null);
+INSERT INTO `permission` VALUES ('6', 'USER', '/user4', null);
+INSERT INTO `permission` VALUES ('7', 'DEV', '/dev', null);
 
 -- ----------------------------
 -- Table structure for role
@@ -41,13 +46,14 @@ CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
 INSERT INTO `role` VALUES ('1', 'ROLE_ADMIN');
 INSERT INTO `role` VALUES ('2', 'ROLE_USER');
+INSERT INTO `role` VALUES ('3', 'ROLE_DEV');
 
 -- ----------------------------
 -- Table structure for role_permission
@@ -62,14 +68,19 @@ CREATE TABLE `role_permission` (
   KEY `permission_id` (`permission_id`),
   CONSTRAINT `role_permission_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
   CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role_permission
 -- ----------------------------
-INSERT INTO `role_permission` VALUES ('1', '1', '2');
-INSERT INTO `role_permission` VALUES ('2', '2', '1');
-INSERT INTO `role_permission` VALUES ('3', '1', '1');
+INSERT INTO `role_permission` VALUES ('1', '1', '1');
+INSERT INTO `role_permission` VALUES ('2', '1', '2');
+INSERT INTO `role_permission` VALUES ('3', '2', '1');
+INSERT INTO `role_permission` VALUES ('4', '2', '3');
+INSERT INTO `role_permission` VALUES ('5', '2', '3');
+INSERT INTO `role_permission` VALUES ('6', '2', '5');
+INSERT INTO `role_permission` VALUES ('7', '2', '6');
+INSERT INTO `role_permission` VALUES ('8', '3', '7');
 
 -- ----------------------------
 -- Table structure for user
@@ -82,13 +93,14 @@ CREATE TABLE `user` (
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'Liu', 'Jinlong', 'liujinlong', '123456');
-INSERT INTO `user` VALUES ('2', 'Tian', 'Haitang', 'tianhaitang', '123456');
+INSERT INTO `user` VALUES ('1', 'Liu', 'Jinlong', 'admin', '123456');
+INSERT INTO `user` VALUES ('2', 'Tian', 'Haitang', 'user', '123456');
+INSERT INTO `user` VALUES ('3', 'dev', 'dev', 'dev', '123456');
 
 -- ----------------------------
 -- Table structure for user_role
@@ -103,10 +115,12 @@ CREATE TABLE `user_role` (
   KEY `role_id` (`role_id`),
   CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_role
 -- ----------------------------
 INSERT INTO `user_role` VALUES ('1', '1', '1');
 INSERT INTO `user_role` VALUES ('2', '2', '2');
+INSERT INTO `user_role` VALUES ('3', '2', '3');
+INSERT INTO `user_role` VALUES ('4', '3', '3');
