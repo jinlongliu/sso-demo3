42f85fa23331a77edf773f6b09dfab57f55043c9
liujinlong <liujinlong@jiudaotech.com>
2017-08-25 14:23:44 +0800

    清除注释代码

--- src/main/java/org/playchain/demo3/DemoApplication.java
+++ src/main/java/org/playchain/demo3/DemoApplication.java
@@ -5,41 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
-//@RestController
-//@EnableResourceServer
-//@EnableAuthorizationServer
+@EnableAuthorizationServer
 @MapperScan("org.playchain.demo3.mapper")
 public class DemoApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-
-/*	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.formLogin().loginPage("/login").permitAll().failureUrl("/login")
-				.and()
-				.csrf().disable()
-				.authorizeRequests()
-//				.antMatchers("/user").permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.logout()
-				.deleteCookies("JSESSIONID")
-				.invalidateHttpSession(true)
-				.logoutUrl("/logout")
-				.logoutSuccessUrl("/login?logout")
-				.and()
-				.httpBasic();
-	}*/
-
-/*
-	@Autowired
-	public void configGobal(AuthenticationManagerBuilder auth) 	throws Exception {
-		auth.inMemoryAuthentication().withUser("bar").password("barsecret").roles("USER");
-	}
-*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
